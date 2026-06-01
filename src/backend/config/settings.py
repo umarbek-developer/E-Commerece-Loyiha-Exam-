67988736eff5c43d5ade5104dd09d0140a64ab06
Umarsobirov <umarbeksobirov0402@mail.ru>
2026-06-01 16:43:49 +0500

    codes

--- src/backend/config/settings.py
+++ src/backend/config/settings.py
@@ -97,7 +97,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=10),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
 }
 
 
